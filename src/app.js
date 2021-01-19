91e9d9bbfb52b42747e003f7f31e03d68ed36184
Simon Panek <paneks19@gmail.com>
2021-01-18 20:05:28 -0800

    functionalize app and index

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import ToDo from './components/todo/todo-connected.js';
 
-export default class App extends React.Component {
-  render() {
-    return (
+const App = () => {
+  return (
       <>
         <ToDo />
       </>
     );
-  }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './app.js';
 
-class Main extends React.Component {
-  render() {
-    return <App />;
-  }
+const Main = () => {
+  return <App />;
 }
 
 const rootElement = document.getElementById('root');
