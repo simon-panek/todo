80d95454caff0445b8751b2a45d4448c68f403f0
Simon Panek <paneks19@gmail.com>
2021-01-18 21:15:38 -0800

    nav bar added and styled

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import ToDo from './components/todo/todo-connected.js';
+import NavBar from './components/todo/nav.js';
 
 const App = () => {
   return (
       <>
+        <NavBar />
         <ToDo />
       </>
     );
