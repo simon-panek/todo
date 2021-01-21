704927a96eb75c6bfb61acea5a72cddf8a17aece
Simon Panek <paneks19@gmail.com>
2021-01-20 23:29:41 -0800

    Updates

--- precache-manifest.843c414540a1f0042d5fb09db0678e40.js
+++ precache-manifest.843c414540a1f0042d5fb09db0678e40.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aae71e42102b45d93c4be0241ada5669",
+    "revision": "1b7f0f1f175c39f48ba8989d3e0e08be",
     "url": "/todo/index.html"
   },
   {
-    "revision": "fe8661da421c66046af6",
+    "revision": "e2c54f307cc0bd00ba1f",
     "url": "/todo/static/css/2.95258f03.chunk.css"
   },
   {
-    "revision": "9cf82eed48245ea39fad",
+    "revision": "d07553173128e3215e55",
     "url": "/todo/static/css/main.605a1201.chunk.css"
   },
   {
-    "revision": "fe8661da421c66046af6",
-    "url": "/todo/static/js/2.dcc79676.chunk.js"
+    "revision": "e2c54f307cc0bd00ba1f",
+    "url": "/todo/static/js/2.9eb23ddf.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/todo/static/js/2.dcc79676.chunk.js.LICENSE.txt"
+    "url": "/todo/static/js/2.9eb23ddf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9cf82eed48245ea39fad",
-    "url": "/todo/static/js/main.5c0c2550.chunk.js"
+    "revision": "d07553173128e3215e55",
+    "url": "/todo/static/js/main.8fed014a.chunk.js"
   },
   {
     "revision": "4b7c96a078d3292937b1",
