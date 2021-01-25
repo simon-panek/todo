967c463bad8c85e1d92c020422b13e31dcdcc947
Simon Panek <paneks19@gmail.com>
2021-01-24 17:12:37 -0800

    add conditional render to login inputs

--- src/components/todo/login.js
+++ src/components/todo/login.js
@@ -1,18 +1,24 @@
-import reactDomTestUtilsProductionMin from "react-dom/cjs/react-dom-test-utils.production.min";
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import { LoginContext } from '../todo/context/loginContext.js';
+import cookie from 'react-cookies';
 
 function Login(props) {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [role, setRole] = useState('user');
+  const [returningUser, setReturningUser] = useState(false);
 
   const loginContext = useContext(LoginContext);
 
+  let token = cookie.load('auth');
+  useEffect (() => {
+    if(token){
+      setReturningUser(true);
+    }
+  },[setReturningUser])
+
   const handleFormSubmit = (e) => { //send username and pword to context
     e.preventDefault();
-    loginContext.login(userName, password, email);
+    loginContext.login(userName, password);
   }
 
   const handleNameChange = (e) => { //updates userName state on change of input field
@@ -23,29 +29,18 @@ function Login(props) {
     setPassword(e.target.value);
   }
 
-  const handleEmailChange = (e) => { //updates password state on change of input field
-    setEmail(e.target.value);
-  }
-
-  const handleRoleChange = (e) => { //updates password state on change of input field
-    setRole(e.target.value);
-  }
-
-  console.log({userName}, {password}, {email}, {role});
+  console.log({userName}, {password});
 
   return(
-    <form onSubmit={handleFormSubmit}>
-      <input onBlur={handleNameChange} type="text" name="name" placeholder="Name"/>
-      <input onBlur={handlePasswordChange} type="password" name="password" placeholder="Password"/>
-      <input onBlur={handleEmailChange} type="email" name="email" placeholder="Email"/>
-      <label>Role</label>
-      <select onChange={handleRoleChange}>
-        <option value="user">"user"</option>
-        <option value="editor">"editor"</option>
-        <option value="admin">"admin"</option>
-      </select>
-      <button>Enter</button>
-    </form>
+    <div>
+      {returningUser === true ? '' :  
+      <form onSubmit={handleFormSubmit}>
+        <input onBlur={handleNameChange} type="text" name="name" placeholder="Name"/>
+        <input onBlur={handlePasswordChange} type="password" name="password" placeholder="Password"/>
+        <button>Enter</button>
+      </form>
+      }
+    </div>
   )
 }
 
